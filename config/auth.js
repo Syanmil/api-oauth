1f73d7b1387f81d3c1dbb8d3085a2acb923465d0
Izhharuddin Syanmil <syanmil@yahoo.com>
2017-02-01 10:29:21 +0700

    Delete auth.js

--- config/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  'twitterAuth' : {
-    'consumerKey'       : 'FvRDdHn41B5BUwy0fd21vVD9M',
-    'consumerSecret'    : 'XEY6xVI8J6DUyOjmfkYiqKBmDr0MeZ95PWwo2NTr9o2aGBzFQq',
-    'callbackURL'       : 'http://localhost:3000/auth/twitter/callback'
-  },
-}
